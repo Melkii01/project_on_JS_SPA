580f4f42a6821e8c4b4196128fa4cb73b3348515
Lenar Mirzayanov <melkiy-dude01@yandex.ru>
2024-01-13 10:03:28 +0300

    commit 2.4 js modification burger

--- frontend/src/components/burger.js
+++ frontend/src/components/burger.js
@@ -1,15 +1,11 @@
-import {Auth} from "../services/auth";
-
 export class Burger {
     urlRoute = window.location.hash.split('?')[0];
     sidebar = document.getElementById('sidebar');
-    mainElement = document.getElementById('main');
     contentElement = document.getElementById('content');
     burgerOpen = document.getElementById('burger-open');
     burgerClose = document.getElementById('burger-close');
     screenWidth = window.screen.width;
-    links = document.querySelectorAll('.my-link-js');
-
+    isBurgerOpenVisible = false;
 
     constructor() {
 
@@ -19,8 +15,8 @@ export class Burger {
             this.burgerClose.style.display = 'none';
         } else {
             // Показываем бургер и главную страницу, если экран шириной меньше 1024
-            const screenWidth = window.screen.width;
-            if (screenWidth < 1024) {
+            this.screenWidth = window.screen.width;
+            if (this.screenWidth < 1024) {
                 this.burgerOpen.style.display = 'block';
                 this.contentElement.style.display = 'flex';
             }
@@ -33,16 +29,17 @@ export class Burger {
         // Слушаем ширину, показываем бургер и сайдбар, или убираем
         window.addEventListener("resize", () => {
             this.screenWidth = window.screen.width;
-            if (this.screenWidth < 1024) {
+            if (this.screenWidth < 1024 && !this.isBurgerOpenVisible) {
                 this.burgerOpen.style.display = 'block';
                 this.sidebar.style.display = 'none';
                 this.burgerClose.style.display = 'none';
-
-            } else {
+                this.isBurgerOpenVisible = true;
+            }
+            if (this.screenWidth >= 1024 && this.isBurgerOpenVisible) {
                 this.burgerOpen.style.display = 'none';
                 this.sidebar.style.display = 'flex';
                 this.sidebar.style.position = 'sticky';
-
+                this.isBurgerOpenVisible = false;
             }
         });
 
@@ -57,35 +54,33 @@ export class Burger {
 
         // При клике на бургер закрывается меню
         this.burgerClose.onclick = () => {
-            this.burgerOpen.style.display = 'block';
-            this.burgerClose.style.display = 'none';
-            this.sidebar.style.display = 'none';
-            this.sidebar.style.position = 'sticky';
+            this.CommonStyle();
         }
-
 
         // При клике вне меню закрывается меню
         this.contentElement.onclick = () => {
             if (this.screenWidth < 1024 && this.sidebar.style.display === 'flex') {
-                this.burgerOpen.style.display = 'block';
-                this.burgerClose.style.display = 'none';
-                this.sidebar.style.display = 'none';
-                this.sidebar.style.position = 'sticky';
+                this.CommonStyle();
             }
         }
 
         // При клике на ссылки в меню закрывается меню
-        if (this.links) {
-            for (let link of this.links) {
-                link.onclick = () => {
-                    if (this.screenWidth < 1024 && this.sidebar.style.display === 'flex') {
-                        this.burgerOpen.style.display = 'block';
-                        this.burgerClose.style.display = 'none';
-                        this.sidebar.style.display = 'none';
-                        this.sidebar.style.position = 'sticky';
-                    }
+        this.sidebar.onclick = (e) => {
+            const target = e.target;
+            if (target.tagName === 'A' && target.classList.contains('my-link-js')
+                || target.tagName === 'IMG' && target.classList.contains('my-link-img-js')) {
+                if (this.screenWidth < 1024 && this.sidebar.style.display === 'flex') {
+                    this.CommonStyle();
                 }
             }
         }
     }
+
+    CommonStyle() {
+        this.burgerOpen.style.display = 'block';
+        this.burgerClose.style.display = 'none';
+        this.sidebar.style.display = 'none';
+        this.sidebar.style.position = 'sticky';
+    }
+
 }
