4f680816ffbe69f6edd8a6eafe55550d904b5764
Lenar Mirzayanov <melkiy-dude01@yandex.ru>
2024-01-15 15:12:11 +0300

    commit 3.5 modification balance change

--- frontend/src/components/balance.js
+++ frontend/src/components/balance.js
@@ -1,12 +1,40 @@
 import {CustomHttp} from "../services/custom-http.js";
 import config from "../../config/config.js";
+import {Popup} from "../utils/popup";
+import {DeleteItem} from "../utils/delete-item";
 
 export class Balance {
     constructor() {
+        //  Получаем баланс
         this.getBalance();
-        // this.changeBalance(122233)
+
+        // Меняем баланс
+        const cash = document.getElementById('cash');
+        cash.onclick = (e) => {
+            const popup = document.getElementById('popupCash');
+            const inputCash = document.getElementById('inputCash');
+            const popupAgreeButton = document.getElementById('popupCashAgreeButton');
+            const popupCancelButton = document.getElementById('popupCashCancelButton');
+
+            popup.classList.add('popup-active');
+
+            // При подтверждении закрыть окно и отправить новое значение
+            popupAgreeButton.onclick = () => {
+                if (inputCash.value && inputCash.value.match(/^\d+$/)) {
+                    popup.classList.remove('popup-active');
+                    this.changeBalance(inputCash.value)
+                }
+            }
+
+            // При отказе закрыть окно
+            popupCancelButton.addEventListener('click', () => {
+                popup.classList.remove('popup-active');
+            })
+        }
+        // this.changeBalance(11)
     }
 
+    //  Получаем баланс
     async getBalance() {
         try {
             const result = await CustomHttp.request(config.host + '/balance');
@@ -22,11 +50,12 @@ export class Balance {
             } else {
                 throw new Error(result.message);
             }
-        } catch (e){
+        } catch (e) {
             return console.log(e);
         }
     }
 
+    // Меняем баланс
     async changeBalance(money) {
         try {
             const result = await CustomHttp.request(config.host + '/balance', 'PUT', {
@@ -38,7 +67,7 @@ export class Balance {
                 if (result.error || result.message) {
                     throw new Error(result.message);
                 } else {
-                    // Заменяем баланс
+                    // // Заменяем баланс
                     let cash = document.getElementById('cash');
                     cash.innerText = result.balance;
                 }
